ab390acdf961e9d1943c795dad7c54e72785eb48
Kess220 <prodvictorkaio@gmail.com>
2023-08-15 22:40:13 -0300

    GET curtidas, curtir, descurtir

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/index.routes.js";
+import LikeRoutes from "./routes/likes.routes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
+app.use("/", LikeRoutes);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`O servidor está rodando na porta ${port}!`));
+app.listen(port, () =>
+  console.log(`O servidor está rodando na porta ${port}!`)
+);
