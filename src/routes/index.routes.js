ad7b93b8af9b0e17cfb5e8429099a700feadea91
Henrique <henriqueluiz266@gmail.com>
2023-08-18 18:00:31 -0300

    feat: get/trending

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,8 +2,9 @@ import { Router } from "express";
 import likesRouter from "./likes.routes.js";
 import postsRouter from "./posts.routes.js";
 import usersRouter from "./users.routes.js";
+import hashtagsRouter from "./hashtags.routes.js";
 
 const indexRouter = Router();
-indexRouter.use(usersRouter, postsRouter, likesRouter);
+indexRouter.use(usersRouter, postsRouter, likesRouter, hashtagsRouter);
 
 export default indexRouter;
